fadbd537bcb7e338bb33947241850f0fd4ac333d
suraj kale <suraj.kale@betasource.tech>
2022-03-20 18:26:33 +0530

    folder name changes done

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { UsersModule } from './users/users.module';
+import { UsersModule } from './company/company.module';
 import { MongooseModule } from '@nestjs/mongoose';
 import { ConfigModule } from '@nestjs/config';
 import { TeamController } from './team/team.controller';
--- src/company/company.module.ts
+++ src/company/company.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
-import { UsersService } from './users.service';
-import { UsersController } from './users.controller';
-import { UserSchema } from './user.model';
+import { UsersService } from './company.service';
+import { UsersController } from './company.controller';
+import { UserSchema } from './company.model';
 import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
